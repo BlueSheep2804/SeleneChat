84325e06333d29d21e09cc0e9c3b75219ec40e95
BlueSheep <48321445+BlueSheep2804@users.noreply.github.com>
2023-05-10 23:11:55 +0900

    feat: 🎸 チャットイベントを拾って整形するメソッドを追加

--- src/main/java/io/github/bluesheep2804/SeleneChat.java
+++ src/main/java/io/github/bluesheep2804/SeleneChat.java
@@ -1,8 +1,13 @@
 package io.github.bluesheep2804;
 
 import com.google.inject.Inject;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.player.PlayerChatEvent;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.slf4j.Logger;
 
 @Plugin(id = "selenechat", name = "SeleneChat", version = "0.1.0-SNAPSHOT",
@@ -18,5 +23,20 @@ public class SeleneChat {
         this.logger = logger;
 
         logger.info("Hello there! I made my first plugin with Velocity.");
+    }
+
+    @Subscribe
+    public void onPlayerChatEvent(PlayerChatEvent event) {
+        String userName = event.getPlayer().getUsername();
+
+        // デフォルトのイベントを無効化する
+        // クライアントのバージョンが1.19.1以降だとキックされるがUnSignedVelocityで回避できる
+        event.setResult(PlayerChatEvent.ChatResult.denied());
+
+        server.sendMessage(Component.text("<")
+                .append(Component.text(userName).hoverEvent(Component.text(userName)).clickEvent(ClickEvent.suggestCommand("/tell " + userName + " ")))
+                .append(Component.text(">"))
+                .append(Component.text(": ", NamedTextColor.GREEN))
+                .append(Component.text(event.getMessage())));
     }
 }
