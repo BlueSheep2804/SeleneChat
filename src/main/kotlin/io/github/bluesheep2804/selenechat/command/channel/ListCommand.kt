88e2ab5e5789575c0a187b773af4b92f1203a2dc
BlueSheep <48321445+BlueSheep2804@users.noreply.github.com>
2025-01-08 20:09:09 +0900

    feat: 🎸 channel listコマンドを見やすいものに変更

--- src/main/kotlin/io/github/bluesheep2804/selenechat/command/channel/ListCommand.kt
+++ src/main/kotlin/io/github/bluesheep2804/selenechat/command/channel/ListCommand.kt
@@ -1,6 +1,8 @@
 package io.github.bluesheep2804.selenechat.command.channel
 
-import io.github.bluesheep2804.selenechat.SeleneChat
+import io.github.bluesheep2804.selenechat.SeleneChat.channelManager
+import io.github.bluesheep2804.selenechat.SeleneChat.resource
+import io.github.bluesheep2804.selenechat.channel.ChannelData
 import io.github.bluesheep2804.selenechat.player.SeleneChatPlayer
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
@@ -10,15 +12,27 @@ class ListCommand: SubCommand() {
     override val permission: String = "selenechat.channel.list"
 
     override fun execute(sender: SeleneChatPlayer, args: Array<String>): Boolean {
-        val returnMessage = Component.text().append(SeleneChat.resource.command.channelSuccessList)
-        SeleneChat.channelManager.allChannels.forEach { (key, channel) ->
+        val returnMessage = Component.text().append(resource.command.channelSuccessList)
+        channelManager.allChannels.forEach { (_, channel) ->
             if (!channel.visible) return@forEach
             returnMessage.appendNewline()
-                    .append(Component.text("- "))
+                    .append(channelIndicator(sender, channel)).appendSpace()
                     .append(channel.displayName)
-                    .append(Component.text("(${key})", NamedTextColor.GRAY))
+                    .append(Component.text("(${channel.name})", NamedTextColor.GRAY))
         }
         sender.sendCommandResult(returnMessage.build())
         return true
+    }
+
+    private fun channelIndicator(sender: SeleneChatPlayer, channel: ChannelData): Component {
+        if (channelManager.playerChannelMap[sender.uniqueId] == channel.name) {
+            return resource.command.channelSuccessListIndicatorSpeak.hoverEvent(resource.command.channelSuccessListIndicatorSpeakHover)
+        }
+
+        if (channel.playerList.contains(sender.uniqueId.toString())) {
+            return resource.command.channelSuccessListIndicatorJoins.hoverEvent(resource.command.channelSuccessListIndicatorJoinsHover)
+        }
+
+        return resource.command.channelSuccessListIndicator.hoverEvent(resource.command.channelSuccessListIndicatorHover)
     }
 }
